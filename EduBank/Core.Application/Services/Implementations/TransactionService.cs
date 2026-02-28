39f8136166787cea032548f88fd98c0c8aac6029
Матвей))))))) <77619364+russkoepoleexperimentov@users.noreply.github.com>
2026-03-01 04:29:06 +0700

    Credit payment communication

--- EduBank/Core.Application/Services/Implementations/TransactionService.cs
+++ EduBank/Core.Application/Services/Implementations/TransactionService.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using Common.Contracts;
+using Common.Enums;
 using Common.Exceptions;
 using Core.Application.Dtos;
 using Core.Application.Services.Interfaces;
 using Core.Domain;
 using Core.Infrastructure;
 using FluentValidation;
+using MassTransit;
 
 namespace Core.Application.Services.Implementations
 {
@@ -15,20 +17,23 @@ namespace Core.Application.Services.Implementations
         private readonly CoreDbContext _context;
         private readonly IAccountService _accountService;
         private readonly IValidator<CreateTransactionDto> _createValidator;
+        private readonly IRequestClient<ProcessExternalPaymentCommand> _paymentClient;
         private readonly IMapper _mapper;
 
-        public TransactionService(CoreDbContext context, IAccountService accountService, IValidator<CreateTransactionDto> createValidator, IMapper mapper)
+        public TransactionService(CoreDbContext context, IAccountService accountService, IValidator<CreateTransactionDto> createValidator, IMapper mapper, IRequestClient<ProcessExternalPaymentCommand> paymentClient)
         {
             _context = context;
             _accountService = accountService;
             _createValidator = createValidator;
             _mapper = mapper;
+            _paymentClient = paymentClient;
         }
 
         public async Task<TransactionDto> InitializeTransactionAsync(CreateTransactionDto dto, Guid currentUserId)
         {
             _createValidator.ValidateAndThrow(dto);
             var transaction = _mapper.Map<Transaction>(dto);
+            transaction.Id = Guid.NewGuid();
 
             if(transaction.SourceType == TransactionObjectType.Account)
             {
@@ -41,10 +46,15 @@ namespace Core.Application.Services.Implementations
                     var targetAcc = await _accountService.GetAccountFromDbAsync(transaction.TargetId!.Value, null);
                     ApplyTransfer(transaction, sourceAcc, targetAcc);
                 }
-                else if(transaction.TargetType == TransactionObjectType.RealWorld) // снятие денег
+                else if (transaction.TargetType == TransactionObjectType.RealWorld) // снятие денег
                 {
                     ApplyWithdraw(transaction, sourceAcc);
                 }
+                else if (transaction.TargetType == TransactionObjectType.Credit) // платеж по кредиту
+                {
+                    await ApplyCreditPayment(transaction, sourceAcc);
+                }
+                else throw new InvalidOperationException();
             }
             else if (transaction.SourceType == TransactionObjectType.RealWorld)
             {
@@ -55,67 +65,14 @@ namespace Core.Application.Services.Implementations
                     var targetAcc = await _accountService.GetAccountFromDbAsync(transaction.TargetId!.Value, currentUserId);
                     ApplyDeposit(transaction, targetAcc);
                 }
+                else throw new InvalidOperationException();
             }
+            else throw new InvalidOperationException();
 
             _context.Transactions.Add(transaction);
             await _context.SaveChangesAsync();
 
             return _mapper.Map<TransactionDto>(transaction);
-        }
-
-        public async Task<TransactionDto> GetTransactionByIdAsync(Guid id, Guid currentUserId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<TransactionDto> ResolveTransactionAsync(Guid id, TransactionStatus status, string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ApplyTransfer(Transaction transaction, Account source, Account target)
-        {
-            if(source.Balance < transaction.Amount)
-            {
-                transaction.Status = TransactionStatus.Failed;
-                transaction.ResolvedAt = DateTime.UtcNow;
-                transaction.ResolutionMessage = "На счёте недостаточно денег";
-                return;
-            }
-
-            source.Balance -= transaction.Amount;
-            target.Balance += transaction.Amount;
-            transaction.Status = TransactionStatus.Completed;
-            transaction.ResolvedAt = DateTime.UtcNow;
-            transaction.ResolutionMessage = "Перевод выполнен";
-        }
-
-        private void ApplyDeposit(Transaction transaction, Account target)
-        {
-            target.Balance += transaction.Amount;
-            transaction.Status = TransactionStatus.Completed;
-            transaction.ResolvedAt = DateTime.UtcNow;
-            transaction.ResolutionMessage = "Пополнение счёта";
-        }
-
-        private void ApplyWithdraw(Transaction transaction, Account source)
-        {
-            source.Balance -= transaction.Amount;
-            transaction.Status = TransactionStatus.Completed;
-            transaction.ResolvedAt = DateTime.UtcNow;
-            transaction.ResolutionMessage = "Снятие денег со счёта";
-        }
-
-        private async Task<Transaction> GetTransactionFromDbAsync(Guid transactionId)
-        {
-            var account = await _context.FindAsync<Transaction>(transactionId);
-
-            if (account == null)
-            {
-                throw new NotFoundException(nameof(Transaction));
-            }
-
-            return account;
         }
 
         public async Task<DepositFundsResponse> ProcessDepositFund(DepositFundsCommand command)
@@ -141,11 +98,99 @@ namespace Core.Application.Services.Implementations
                 _context.Accounts.Update(targetAcc);
                 await _context.SaveChangesAsync();
             }
-            catch (Exception ex) { 
+            catch (Exception ex)
+            {
                 return new(false, ex.Message);
             }
 
             return new(true, null);
+        }
+
+        public async Task<TransactionDto> GetTransactionByIdAsync(Guid id, Guid currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TransactionDto> ResolveTransactionAsync(Guid id, TransactionStatus status, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task ApplyCreditPayment(Transaction transaction, Account source)
+        {
+            if (!EnsureCanWithdraw(transaction, source)) return;
+
+            var response = await _paymentClient.GetResponse<ProcessExternalPaymentResponse>(
+                new ProcessExternalPaymentCommand(transaction.TargetId!.Value, transaction.Amount, transaction.Id.ToString(), DateTime.UtcNow));
+
+            if (!response.Message.Success)
+            {
+                transaction.Status = TransactionStatus.Failed;
+                transaction.ResolutionMessage = response.Message.Message;
+                transaction.ResolvedAt = DateTime.UtcNow;
+            }
+            else
+            {
+                transaction.Status = TransactionStatus.Completed;
+                transaction.ResolutionMessage = "Оплата кредита";
+                transaction.ResolvedAt = DateTime.UtcNow;
+
+                source.Balance -= transaction.Amount;
+            }
+        }
+
+        private void ApplyTransfer(Transaction transaction, Account source, Account target)
+        {
+            if(!EnsureCanWithdraw(transaction, source)) return;
+
+            source.Balance -= transaction.Amount;
+            target.Balance += transaction.Amount;
+            transaction.Status = TransactionStatus.Completed;
+            transaction.ResolvedAt = DateTime.UtcNow;
+            transaction.ResolutionMessage = "Перевод выполнен";
+        }
+
+        private static bool EnsureCanWithdraw(Transaction transaction, Account source)
+        {
+            if (source.Balance < transaction.Amount)
+            {
+                transaction.Status = TransactionStatus.Failed;
+                transaction.ResolvedAt = DateTime.UtcNow;
+                transaction.ResolutionMessage = "На счёте недостаточно денег";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ApplyDeposit(Transaction transaction, Account target)
+        {
+            target.Balance += transaction.Amount;
+            transaction.Status = TransactionStatus.Completed;
+            transaction.ResolvedAt = DateTime.UtcNow;
+            transaction.ResolutionMessage = "Пополнение счёта";
+        }
+
+        private void ApplyWithdraw(Transaction transaction, Account source)
+        {
+            if (!EnsureCanWithdraw(transaction, source)) return;
+
+            source.Balance -= transaction.Amount;
+            transaction.Status = TransactionStatus.Completed;
+            transaction.ResolvedAt = DateTime.UtcNow;
+            transaction.ResolutionMessage = "Снятие денег со счёта";
+        }
+
+        private async Task<Transaction> GetTransactionFromDbAsync(Guid transactionId)
+        {
+            var account = await _context.FindAsync<Transaction>(transactionId);
+
+            if (account == null)
+            {
+                throw new NotFoundException(nameof(Transaction));
+            }
+
+            return account;
         }
     }
 }
